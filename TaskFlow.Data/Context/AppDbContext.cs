eb77b43ba06ab2321915b49214f6867aed919778
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2026-02-20 18:26:43 +0300

    Install efcore,AddDbContext ayağa kalkmasının sağlanması

---
+++ TaskFlow.Data/Context/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskFlow.Data.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+}
--- TaskFlow/Program.cs
+++ TaskFlow/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Data.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("TaskDistributionDB"));
 
 var app = builder.Build();
 
