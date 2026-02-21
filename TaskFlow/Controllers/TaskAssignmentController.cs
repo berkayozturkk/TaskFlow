94b3b170c6a5c6ae398e74d962cc452ab60a8748
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2026-02-21 18:43:59 +0300

    Controller düzenlemesi ve yeni menü isimleri

---
+++ TaskFlow/Controllers/TaskAssignmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskFlow.Controllers
+{
+    public class TaskAssignmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TaskFlow/Controllers/TaskDefinitionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskFlow.Controllers
+{
+    public class TaskDefinitionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
